b15e4ead71f2adb072d56c557c647ca435dd3c28
VitorPaulon <vtrpaulon@gmail.com>
2026-01-06 20:18:53 -0300

    finalizando o projeto

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 //@vitorPaulon
-let nomeDoHeroi = "SuperPapel";
+let nomeDoHeroi = "SuperVitor";
 let xp = 15000;
 
-if (xp <= 1000){
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Ferro`)
-}else if (xp <= 2000){
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Bronze`)
-}else if (xp <= 5000){
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Prata`)
-}else if (xp <= 7000){
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Ouro`)
-}else if (xp <= 8000){
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Platina`)
-}else if (xp <= 9000){
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Ascendente`)
-}else if (xp <= 10000){
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Imortal`)
-}else{
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Radiante`)   
+function calculadoraDeXP(acoesConcluidas){
+
+    if (xp <= 1000){
+    return `O Herói de nome ${nomeDoHeroi} está no nível de Ferro`
+    }else if (xp <= 2000){
+        return `O Herói de nome ${nomeDoHeroi} está no nível de Bronze`
+    }else if (xp <= 5000){
+        return `O Herói de nome ${nomeDoHeroi} está no nível de Prata`
+    }else if (xp <= 7000){
+        return `O Herói de nome ${nomeDoHeroi} está no nível de Ouro`
+    }else if (xp <= 8000){
+        return `O Herói de nome ${nomeDoHeroi} está no nível de Platina`
+    }else if (xp <= 9000){
+        return `O Herói de nome ${nomeDoHeroi} está no nível de Ascendente`
+    }else if (xp <= 10000){
+        return `O Herói de nome ${nomeDoHeroi} está no nível de Imortal`
+    }else{
+        return `O Herói de nome ${nomeDoHeroi} está no nível de Supremo`   
+    }
 }
+console.log(calculadoraDeXP(xp));
